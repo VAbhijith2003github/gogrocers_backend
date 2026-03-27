ba65d604d1b506e47fae42bd3e08dc1b254a1d00
V Abhijith <119759593+VAbhijith2003github@users.noreply.github.com>
2026-03-27 21:43:33 +0530

    kafka fix

--- mailer-service/src/config/kafka.js
+++ mailer-service/src/config/kafka.js
@@ -2,7 +2,7 @@ const { Kafka } = require("kafkajs");
 
 const kafka = new Kafka({
   clientId: "gogrocers-mailer",
-  brokers: [process.env.KAFKA_BROKERS],
+  brokers: process.env.KAFKA_BROKERS ? process.env.KAFKA_BROKERS.split(',').map(b => b.trim().replace(/['"]/g, '')) : [],
   ssl: true,
   sasl: {
     mechanism: process.env.KAFKA_SASL_MECHANISM || "scram-sha-256",
